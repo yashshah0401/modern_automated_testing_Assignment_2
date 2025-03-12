7812f0daafaa8dc39996629443960b4a1f2a06f4
yashshah0401 <yshah0493@conestogac.on.ca>
2025-03-12 15:01:26 -0400

    Add test case for both inputs being zero

--- SENG8120-Modern-Automated-Testing-evaluating-for-test-coverage/src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ SENG8120-Modern-Automated-Testing-evaluating-for-test-coverage/src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -20,4 +20,14 @@ describe("calculateCanvasSize", () => {
     const result = calculateCanvasSize("0", "100");
     expect(result).toEqual(0);
   });
+
+  test("returns correct result for large numeric inputs", () => {
+    const result = calculateCanvasSize("100000", "100000");
+    expect(result).toEqual(10000000000);
+  });
+
+  test("returns 0 for both inputs being zero", () => {
+    const result = calculateCanvasSize("0", "0");
+    expect(result).toEqual(0);
+  })
 });
